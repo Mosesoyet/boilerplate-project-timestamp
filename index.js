3e19d24fe1ab32ee5ec1974ec95f820142638213
= <=>
2023-05-23 19:45:48 +0200

    Create the /api/date route and implement the function to check for valid date and return a formated date

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+require("dotenv").config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -24,9 +25,105 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// a function to get exact date string
 
+let getDay = (day) => {
+  switch(day) {
+    case 1:
+      return "Sun";
+      break;
+    case 2:
+      return "Mon";
+      break;
+    case 3:
+      return "Tue";
+      break;
+    case 4:
+      return "Wed";
+      break;
+    case 5:
+      return "Thus";
+      break;
+    case 6:
+      return "Fri";
+      break;
+    case 7:
+      return "Sat"
+      break;
+  }
+  return day;
+}
 
-// listen for requests :)
+// Get month string
+
+let getMonth = (month) => {
+  switch(month) {
+    case 1:
+      return "Jan";
+      break;
+    case 2:
+      return "Feb"
+      break;
+    case 3:
+      return "Mar";
+      break;
+    case 4:
+      return "Apr";
+      break;
+    case 5:
+      return "May";
+      break;
+    case 6:
+      return "Jun";
+      break;
+    case 7:
+      return "July";
+      break;
+    case 8:
+      return "Aug";
+      break;
+    case 9:
+      return "Sept";
+      break;
+    case 10:
+      return "Oct";
+      break;
+    case 11:
+      return "Nov";
+      break;
+    case 12:
+      return "Dec";
+      break;
+  }
+  return month;
+}
+
+app.get('/api/:date', (req,res,next) => {
+  req.date = req.params.date;
+  next()
+},(req,res) => {
+  if(req.date === "") {
+    let dateNow = new Date().toString();
+    res.json({"unix": dateNow})
+  } else if(isNaN(req.date)) {
+    res.json({ "error" : "Invalid Date"})
+  } else {
+    let d = new Date(req.date * 1000)
+    let day = d.getDay();
+    let dateN = d.getDate();
+    let month = d.getMonth();
+    let year = d.getFullYear();
+    let hour = d.getHours()
+    let mins = d.getMinutes()
+    let sec = d.getSeconds()
+    res.json({
+      "unix" : req.date,
+      "utc": `${getDay(day)}, ${dateN} ${getMonth(month)} ${year} ${hour}:${mins}:${sec} GMT`
+    })
+  }
+})
+
+// listen for requests :
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
